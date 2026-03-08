360a204ca87f5a92be0198c88964679b726ea4cd
mohamedelawakey <mohamedelawakey@gmail.com>
2026-03-08 23:43:19 +0200

    refactor: update embeddings import path in package init

--- pipeline/__init__.py
+++ pipeline/__init__.py
@@ -2,7 +2,7 @@ from utils.logger import get_logger
 from .config import Config
 from .cleaning.cleaner import Cleaner
 from .chunking.chunker import TextSplitter
-from .embedding import Embedding
+from .embeddings import Embedding
 from .parser.parser import DocumentExtractor
 
 __all__ = ["Config", "Cleaner", "TextSplitter", "Embedding", "DocumentExtractor", "get_logger"]
