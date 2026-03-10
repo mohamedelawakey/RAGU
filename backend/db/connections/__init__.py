37ce484935980a6aaaa3b79df23c24b48b9bd9fb
mohamedelawakey <mohamedelawakey@gmail.com>
2026-03-10 20:51:16 +0200

    chore(backend/db): clean up exported modules in __init__ files

--- backend/db/connections/__init__.py
+++ backend/db/connections/__init__.py
@@ -1,10 +1,8 @@
-from utils.logger import get_logger
 from .postgres import PostgresDBConnection
 from .redis import AsyncRedisDBConnection
 from .milvus import AsyncMilvusDBConnection
 
 __all__ = [
-    "get_logger",
     "PostgresDBConnection",
     "AsyncRedisDBConnection",
     "AsyncMilvusDBConnection",
