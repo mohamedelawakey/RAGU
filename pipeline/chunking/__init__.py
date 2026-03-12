3ee50a6fba2c0965990f818fad54e8521b00dfc9
mohamedelawakey <mohamedelawakey@gmail.com>
2026-03-12 22:49:37 +0200

    chore(parser): remove uselessd imports in init

--- pipeline/chunking/__init__.py
+++ pipeline/chunking/__init__.py
@@ -1,5 +1,5 @@
-from utils.logger import get_logger
-from pipeline.config import Config
 from .chunker import TextSplitter
 
-__all__ = ["TextSplitter", "get_logger", "Config"]
+__all__ = [
+    "TextSplitter"
+]
