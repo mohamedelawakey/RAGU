c5bd5e84ff7c96a8b385875995eccf249db1d427
mohamedelawakey <mohamedelawakey@gmail.com>
2026-03-12 20:15:45 +0200

    refactor(search): finalize search module exports

--- pipeline/search/__init__.py
+++ pipeline/search/__init__.py
@@ -1,11 +1,9 @@
 from .semantic_search import SemanticSearch
-from .bm25_search import BM25Search
 from .hybrid_search import HybridSearch
-from utils.logger import get_logger
-from config import Config
+from .bm25_search import BM25Search
 
 __all__ = [
     "SemanticSearch",
+    "HybridSearch",
     "BM25Search",
-    "HybridSearch"
 ]
