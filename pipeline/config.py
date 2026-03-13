38fff2a7ef714ee2b1e078c798d24c4b7e478c68
mohamedelawakey <mohamedelawakey@gmail.com>
2026-03-13 23:21:11 +0200

    update: add PostgreSQL & BM25 & Semantic Search & Hybrid Search configurations

--- pipeline/config.py
+++ pipeline/config.py
@@ -22,3 +22,48 @@ class Config:
     MULTIPLIER = 1
     RETRY_MIN_WAIT = 2
     RETRY_MAX_WAIT = 10
+
+    # Insert Document Node into Postgres
+    INSERT_DOCUMENT_NODE = """
+        INSERT INTO documents (filename, file_path, status)
+        VALUES ($1, $2, 'completed')
+        RETURNING id
+    """
+
+    # insert chunks into Postgres
+    INSERT_CHUNKS = """
+        INSERT INTO document_chunks (id, document_id, page_number, text_content)
+        VALUES ($1, $2, $3, $4)
+    """
+
+    # BM25 Search Configurations (Advanced Multilingual & Fuzzy Search)
+    BM25_TOP_K = 5
+    BM25_QUERY = """
+        WITH search_query AS (
+            SELECT websearch_to_tsquery('simple', $1) AS q
+        )
+        SELECT 
+            c.id as chunk_id, 
+            (
+                ts_rank_cd(to_tsvector('simple', c.text_content), sq.q) * 0.7 +
+                similarity(c.text_content, $1) * 0.3
+            ) as rank_score
+        FROM document_chunks c
+        CROSS JOIN search_query sq
+        WHERE 
+            to_tsvector('simple', c.text_content) @@ sq.q
+            OR c.text_content % $1
+        ORDER BY rank_score DESC
+        LIMIT $2
+    """
+
+    # Semantic Search Configurations
+    SEMANTIC_SEARCH_TOP_K = 5
+    COLLECTION_NAME = "edu_chunks"
+    SEARCH_PARAMS_METRIC_TYPE = "COSINE"
+    SEARCH_PARAMS_NPROBE = 10
+    MILVUS_ALIAS = "default"
+
+    # Hybrid Search Configurations
+    HYBRID_SEARCH_TOP_K = 5
+    RRF_K = 60
