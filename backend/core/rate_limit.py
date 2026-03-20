2054e2dda38d4c03179991d90b385e071700ade5
mohamedelawakey <mohamedelawakey@gmail.com>
2026-03-20 04:31:47 +0200

    feat(core): initialize FastAPI rate limiter with Redis backend

---
+++ backend/core/rate_limit.py
@@ -0,0 +1,19 @@
+from backend.db.connections.redis import AsyncRedisDBConnection
+from fastapi_limiter import FastAPILimiter
+from utils.logger import get_logger
+
+logger = get_logger("core.rate_limit")
+
+
+async def init_rate_limiter():
+    try:
+        redis_pool = AsyncRedisDBConnection.get_redis()
+        await FastAPILimiter.init(redis_pool)
+
+        logger.info(
+            "FastAPI rate limiter successfully initialized with Redis."
+        )
+
+    except Exception as e:
+        logger.error(f"Failed to initialize global rate limiter: {e}")
+        raise
